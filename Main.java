80e4774ed2f434b760e63f8caf85012ae56ce2ac
fridefroland <fridef@stud.ntnu.no>
2018-04-09 13:25:51 +0200

    main and userinterface

--- Main.java
+++ Main.java
@@ -1,33 +1,18 @@
 
 /**
- * Write a description of class Main here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * Represents a collections of newspapers.
+ * @author Geir Kvenseth, Fride frøland, Helene Rasmussen
+ * @version (12.02.2018)
  */
 public class Main
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
     /**
-     * Constructor for objects of class Main
+     * The main entry for the application.
+     * @param args arguments provided during startup of the application
      */
-    public Main()
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        UserInterface appUI = new UserInterface();
+        appUI.start();
     }
 }
--- UserInterface.java
+++ UserInterface.java
@@ -1,33 +1,177 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.ArrayList;
 
 /**
  * Write a description of class UserInterface here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Geir Kvenseth, Fride frøland, Helene Rasmussen
+ * @version (12.02.2018)
  */
 public class UserInterface
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private NewspaperOrganizer newspaperOrganizer;
+    private String[] menuItems = {
+            "1. List all newspapers",
+            "2. Add new newspaper",
+            "3. Find a newspaper by name",
+            "4. For later use",
+        };
 
-    /**
-     * Constructor for objects of class UserInterface
-     */
     public UserInterface()
     {
-        // initialise instance variables
-        x = 0;
+        newspaperOrganizer = new NewspaperOrganizer();
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Starts the application by showing the menu and retrieving input from the
+     * user.
      */
-    public int sampleMethod(int y)
+    public void start() 
     {
-        // put your code here
-        return x + y;
+        this.init();
+
+        boolean quit = false;
+
+        while (!quit) 
+        {
+            try 
+            {
+                int menuSelection = this.showMenu();
+                switch (menuSelection) 
+                {
+                    case 1:
+                    this.listAllNewspapers();
+                    break;
+
+                    case 2:
+                    addNewspaper();
+                    break;
+
+                    case 3:
+                    this.findNewspaperByName();
+                    break;
+
+                    case 5:
+                    System.out.println("\nThank you for using Application v0.1. Bye!\n");
+                    quit = true;
+                    break;
+
+                    default:
+                }
+            } 
+            catch (InputMismatchException ime) 
+            {
+                System.out.println("\nERROR: Please provide a number between 1 and " + this.menuItems.length + "..\n");
+            }
+        }        
+
+    }
+
+    /**
+     * Initializes the application.
+     * Typically you would create the LiteratureRegistrer-instance here
+     */
+    private void init()
+    {
+        System.out.println("init() was called");
+    }
+
+    /**
+     * Displays the menu to the user, and waits for the users input. The user is
+     * expected to input an integer between 1 and the max number of menu items. 
+     * If the user inputs anything else, an InputMismatchException is thrown. 
+     * The method returns the valid input from the user.
+     *
+     * @return the menu number (between 1 and max menu item number) provided by the user.
+     * @throws InputMismatchException if user enters an invalid number/menu choice
+     */
+    private int showMenu() throws InputMismatchException 
+    {
+        System.out.println("\n**** Application v0.1 ****\n");
+        // Display the menu
+        for ( String menuItem : menuItems )
+        {
+            System.out.println(menuItem);
+        }
+        int maxMenuItemNumber = menuItems.length + 1;
+        // Add the "Exit"-choice to the menu
+        System.out.println(maxMenuItemNumber + ". Exit\n");
+        System.out.println("Please choose menu item (1-" + maxMenuItemNumber + "): ");
+        // Read input from user
+        Scanner reader = new Scanner(System.in);
+        int menuSelection = reader.nextInt();
+        if ((menuSelection < 1) || (menuSelection > maxMenuItemNumber)) 
+        {
+            throw new InputMismatchException();
+        }
+        return menuSelection;
+    }
+
+    /**
+     * Adds a newspaper to the newspaperorganizer.
+     * @param newspaper The newspaper to add
+     */
+    public void addNewspaper()
+    {
+        Scanner inputScanner = new Scanner(System.in);
+        
+        System.out.println("Enter the name: ");
+        String name = inputScanner.nextLine();
+
+        System.out.println("Enter the date of publishing: ");
+        String date = inputScanner.nextLine();
+
+        System.out.println("Enter the number of pages in the newpaper: ");
+        int numberOfPages = inputScanner.nextInt();
+
+        System.out.println("Enter the issuenumber: ");
+        int issueNumber = inputScanner.nextInt();
+
+        newspaperOrganizer.addNewspaper(name, issueNumber, date, numberOfPages);
+    }
+
+    /**
+     * Lists all the products/literature in the register
+     */
+    public void listAllNewspapers()
+    {
+        Iterator<Newspaper> it = newspaperOrganizer.iterator();
+        if (!it.hasNext())
+        {
+            System.out.println("There are no newspapers in stock!");
+        }
+        else
+        {
+            System.out.println("Newspapers in stock are:");
+            while(it.hasNext()){
+                Newspaper n = it.next();
+                System.out.println(n.getName());
+            }
+        }
+    }
+
+    /**
+     * Find and return the newspaper with a name matching the parameter name.
+     * @param name The name of the newspaper to search for.
+     */
+    public void findNewspaperByName()
+    {
+        Scanner inputScanner = new Scanner(System.in);
+        
+        System.out.println("Enter the name:");
+        String searchString = inputScanner.nextLine();
+        System.out.println();
+
+        Iterator<Newspaper> it = newspaperOrganizer.iterator();
+        while (it.hasNext())
+        {
+            Newspaper n = it.next();
+            if (n.getName().equalsIgnoreCase(searchString))
+            {
+                System.out.println(n.toString());
+            }
+        }
     }
 }
